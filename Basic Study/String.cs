ca7595ccee931f07aad60d9ee406ff8fed6b6ef1
yangfaring <did030930@gmail.com>
2024-08-19 11:31:32 +0900

    Update String.cs

--- Basic Study/String.cs
+++ Basic Study/String.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,38 +16,62 @@ namespace String
         {
             InitializeComponent();
         }
+        //추가
+        StringBuilder str = new StringBuilder();
+
         /// <summary>
-        /// String 클래스의 Length,Substring 20240814
+        /// String 클래스의 Length,Substring 20240814 / Replace,ToUpper, ToLower,StringBuilder 20240819
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnInput_Click(object sender, EventArgs e)
         {
-           //substring 
+
             string str = tbInput.Text;
-            string subStr = str.Substring(7, 1);
-            string subMonth = str.Substring(2, 2);
-            string subDay = str.Substring(4, 2);
 
-            lblBirthday.Text = "생일:" + subMonth + "월" + subDay + "일";
+            // 앞글자만 대문자
+            //string str1 = str.Substring(0, 1);
+            //string str2 = str.Substring(1,str.Length -1);
+
+            //str1 = str1.ToUpper();
+            //str2 = str2.ToLower();
+
+            //lblResult.Text = str1 + str2;
 
 
-            if ((subStr == "1") || (subStr == "3"))
-            {
-                lblResult.Text = "남성";
-            }
-            else if ((subStr == "2") || (subStr == "4"))
-            {
-                lblResult.Text = "여성";
+            //ToUpper
+            //str = str.ToUpper();
+            //lblResult.Text = str;
 
-            }
-            else
-            {
-                lblResult.Text = "당신은 외계인?";
-             
-            }
-            
-            
+
+            //Replace
+            //string str2 = str.Replace(" ", "");
+            //lblResult.Text = str2;
+
+            //substring 
+            //string subStr = str.Substring(7, 1);
+            //string subMonth = str.Substring(2, 2);
+            //string subDay = str.Substring(4, 2);
+
+            //lblBirthday.Text = "생일:" + subMonth + "월" + subDay + "일";
+
+
+            //if ((subStr == "1") || (subStr == "3"))
+            //{
+            //    lblResult.Text = "남성";
+            //}
+            //else if ((subStr == "2") || (subStr == "4"))
+            //{
+            //    lblResult.Text = "여성";
+
+            //}
+            //else
+            //{
+            //    lblResult.Text = "당신은 외계인?";
+
+            //}
+
+
             //length
             /*lblResult.Text = str.Length.ToString();
 
@@ -58,5 +82,31 @@ namespace String
                 lblResult.Text = (pos + 1) + "번째에 - 문자가 있습니다.";
             }*/
         }
+        //StringBuilder
+        private void btnDefault_Click(object sender, EventArgs e)
+        {
+            str.Clear();
+            str.Append("안녕하세요");
+            lblResult.Text = str.ToString();
+        }
+
+        private void btnAppend_Click(object sender, EventArgs e)
+        {
+            str.Append("Hello");
+            lblResult.Text = str.ToString();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            str.Remove(2, 3);
+            lblResult.Text = str.ToString();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            str.Insert(2, "영어로는");
+            lblResult.Text = str.ToString();
+        }
+
     }
 }
