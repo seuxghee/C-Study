6cb65361a1720b6f7e16eeee979d8a8884c04c74
yangfaring <did030930@gmail.com>
2024-08-19 16:39:02 +0900

    20240819

---
+++ Basic Study/Box.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace String_2
+{
+    public partial class Box : Form
+    {
+        public Box()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 메시지박스, 체크박스 사용하기/ 라디오버튼, 콤보박스,리스트박스 사용하기 20240819
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("버튼을 클릭했습니다.","메세지박스제목",MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+        }
+        // 메시지 박스, 체크박스 사용하기
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            string strOrder = "";
+            lblOrder.Text = "";
+
+            if (checkBox1.Checked == true)
+            {
+                strOrder += checkBox1.Text + "\n";
+            }
+            if (checkBox2.Checked == true)
+            {
+                strOrder += checkBox2.Text + "\n";
+            }
+            if (checkBox3.Checked == true)
+            {
+                strOrder += checkBox3.Text + "\n";
+            }
+            if (checkBox4.Checked == true)
+            {
+                strOrder += checkBox4.Text + "\n";
+            }
+
+            lblOrder.Text = strOrder + "메뉴를 주문하셨습니다.";
+
+            if(!checkBox1.Checked  && !checkBox2.Checked && !checkBox3.Checked && !checkBox4.Checked)
+            {
+                MessageBox.Show("메뉴를 선택하고 주문하세요!", "주문 오류", MessageBoxButtons.OK ,MessageBoxIcon.Warning);
+            }
+        }
+        // 라디오버튼, 콤보박스, 리스트박스
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (rbAgree.Checked)
+            {
+                MessageBox.Show("이벤트에 응모하셨습니다.", "이벤트 응오 결과");
+            }
+            else
+            {
+                MessageBox.Show("개인정보에동의하지 않으셨습니다.");
+            }
+        }
+
+        private void btnRequest_Click(object sender, EventArgs e)
+        {
+            string strText = textBox1.Text + "\n라고 추가요구사항이 접수되었습니다.";
+            MessageBox.Show(strText, "추가요구사항");
+        }
+
+        private void btnPay_Click(object sender, EventArgs e)
+        {
+            string strText = cbPay.Text + " " + lbPayInfo.Text
+                + "\n결제방법을 선택하셨습니다.";
+            MessageBox.Show(strText, "결제정보확인");
+        }
+
+
+        private void cbPay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbPayInfo.Items.Clear();
+
+            if (cbPay.SelectedIndex == 0)
+            {
+                lbPayInfo.Items.Add("일시불");
+                lbPayInfo.Items.Add("3개월할부");
+                lbPayInfo.Items.Add("6개월할부");
+
+            }
+            else if (cbPay.SelectedIndex == 1)
+            {
+                lbPayInfo.Items.Add("우리은행");
+                lbPayInfo.Items.Add("국민은행");
+                lbPayInfo.Items.Add("하나은행");
+
+            }
+            else if (cbPay.SelectedIndex == 2)
+            {
+                lbPayInfo.Items.Add("롯데포인트");
+                lbPayInfo.Items.Add("OK캐쉬백");
+                lbPayInfo.Items.Add("주유포인트");
+
+            }
+        }
+    }
+}
---
+++ Basic Study/Date.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Date
+{
+    public partial class Date : Form
+    {
+        public Date()
+        {
+            InitializeComponent();
+            
+        }
+        /// <summary>
+        /// 날짜 시간 정하기 20240819
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDateTime.Text = DateTime.Now.ToString();
+            laDT2.Text = DateTime.Now.ToLongDateString();
+            laDT3.Text = DateTime.Now.ToShortDateString();
+            laDT4.Text = DateTime.Now.ToLongTimeString();
+            laDT5.Text = DateTime.Now.ToShortTimeString();
+            laDT6.Text = "현재시간은" + DateTime.Now.Hour + "시" 
+                + DateTime.Now.Minute + "분 입니다.";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ledRun.BackColor = Color.Lime;
+            ledRun.Text = "ON";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ledRun.BackColor = Color.White;
+            ledRun.Text = "OFF";
+        }
+    }
+}
