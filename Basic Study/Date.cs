56c93d5045011011df1610b09107caf9be083b8f
yangfaring <did030930@gmail.com>
2024-08-20 14:42:25 +0900

    Update Date.cs

--- Basic Study/Date.cs
+++ Basic Study/Date.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,8 +17,12 @@ namespace Date
             InitializeComponent();
             
         }
+
+        Random rdNum = new Random(); //랜덤클래스
+
         /// <summary>
-        /// 날짜 시간 정하기 20240819
+        /// 날짜 시간 정하기 20240819 
+        /// 랜덤 클래스, 새창열기 20240820
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -38,12 +42,38 @@ namespace Date
         {
             ledRun.BackColor = Color.Lime;
             ledRun.Text = "ON";
+            timer2.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             ledRun.BackColor = Color.White;
             ledRun.Text = "OFF";
+            timer2.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int num = rdNum.Next(0, 100);
+            tbCh1.Text =num.ToString();
+
+            if (num >= 50)
+            {
+                ledCh1.BackColor = Color.Orange;
+                ledCh1.Text = "ON";
+            }
+            else
+            {
+                ledCh1.BackColor = Color.Gray;
+                ledCh1.Text = "OFF";
+            }
+        }
+        //새창열기
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+          //  f2.Show(); 
+            f2.ShowDialog(); //닫아야지 다른화면 사용가능함
         }
     }
 }
