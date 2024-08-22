ddfbd0c1d87d6d41bbe69ef6cfccc4711ca37af9
yangfaring <did030930@gmail.com>
2024-08-22 16:02:08 +0900

    간단한 게임 만들기

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.healthbar = new System.Windows.Forms.Label();
+            this.coin = new System.Windows.Forms.PictureBox();
+            this.score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.YellowGreen;
+            this.player.Location = new System.Drawing.Point(120, 288);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(48, 50);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightCoral;
+            this.pictureBox1.Location = new System.Drawing.Point(536, 288);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "obstacle";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // healthbar
+            // 
+            this.healthbar.AutoSize = true;
+            this.healthbar.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.healthbar.Location = new System.Drawing.Point(12, 9);
+            this.healthbar.Name = "healthbar";
+            this.healthbar.Size = new System.Drawing.Size(87, 20);
+            this.healthbar.TabIndex = 2;
+            this.healthbar.Text = "체력 : 0";
+            // 
+            // coin
+            // 
+            this.coin.BackColor = System.Drawing.Color.Khaki;
+            this.coin.Location = new System.Drawing.Point(268, 315);
+            this.coin.Name = "coin";
+            this.coin.Size = new System.Drawing.Size(24, 23);
+            this.coin.TabIndex = 3;
+            this.coin.TabStop = false;
+            this.coin.Tag = "coin";
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.score.Location = new System.Drawing.Point(601, 20);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(87, 20);
+            this.score.TabIndex = 2;
+            this.score.Text = "점수 : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 533);
+            this.Controls.Add(this.coin);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.healthbar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label healthbar;
+        private System.Windows.Forms.PictureBox coin;
+        private System.Windows.Forms.Label score;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {  
+        /// <summary>
+        /// 간단한 게임 만들기 20240822
+        /// </summary>
+
+        int health = 100;
+        int playerspeed = 7;
+        int damage = 20;
+        int scorepoint = 0;
+
+        bool goleft, goright; //참, 거짓을 나타냄
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        //게임 메인 타이머
+        private void timer1_Tick(object sender, EventArgs e) 
+        {
+            healthbar.Text = "체력 : " + health; //초기 체력 설정
+            score.Text = "점수 :" + scorepoint; //점수 설정
+            if (goleft)
+            {
+                player.Left -= playerspeed;
+            } 
+            if (goright)
+            {
+                player.Left += playerspeed;
+            }
+
+            
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+            
+            foreach (Control control in this.Controls)
+            {
+                if (control is PictureBox)
+                {
+                    if ((string)control.Tag == "obstacle")// 장애물 데미지
+                    {
+                        if (player.Bounds.IntersectsWith(control.Bounds) && control.Visible == true)
+                        {
+                            control.Visible = false;
+                            health -= damage;
+                        }
+                    }
+
+                    if ((string)control.Tag == "coin")// 코인
+                    {
+                        if (player.Bounds.IntersectsWith(control.Bounds) && control.Visible == true)
+                        {
+                            control.Visible = false;
+                            scorepoint++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+        }
+    }
+}
